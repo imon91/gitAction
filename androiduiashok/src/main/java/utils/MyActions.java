82725ab79c54d17b24dbf42582e9fac5b81f11e3
ashoka98 <65850226+ashoka98@users.noreply.github.com>
2020-06-08 18:11:11 +0530

    added all modules
    add all objects
    copy dependencieš
    create "androiduiashok" module
    create "appflow" class

---
+++ androiduiashok/src/main/java/utils/MyActions.java
@@ -0,0 +1,25 @@
+package utils;
+
+import io.appium.java_client.android.AndroidElement;
+
+public class MyActions {
+
+    public void action_click(AndroidElement element){
+        element.click();
+    }
+
+    public void action_sendKeys(AndroidElement element, String data){
+        CommonUtils.threadSleep(3000);
+        element.sendKeys(data);
+    }
+
+    public String action_getText(AndroidElement element){
+        CommonUtils.threadSleep(1000);
+        return element.getText();
+    }
+
+    public String action_getTagName(AndroidElement element){
+        CommonUtils.threadSleep(1000);
+        return element.getTagName();
+    }
+}
