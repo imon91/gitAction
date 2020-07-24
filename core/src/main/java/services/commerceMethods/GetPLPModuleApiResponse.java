8271b074c619ba076678cfb458719d7620b77901
ashoka98 <65850226+ashoka98@users.noreply.github.com>
2020-07-25 02:25:57 +0530

    added regressionTestCase inPLP.java(VerificationOfDataAfterFilter)

--- core/src/main/java/services/commerceMethods/GetPLPModuleApiResponse.java
+++ core/src/main/java/services/commerceMethods/GetPLPModuleApiResponse.java
@@ -1,12 +1,12 @@
 package services.commerceMethods;
 
 import com.google.gson.Gson;
-import coreUtils.CoreConstants;
 import io.restassured.response.Response;
-import services.responseModels.commerceModels.ProductListingResultsModel;
+import services.responseModels.commerceModels.*;
 import services.serviceUtils.*;
-
 import java.util.*;
+
+
 
 public class GetPLPModuleApiResponse {
 
@@ -29,6 +29,53 @@ public class GetPLPModuleApiResponse {
                     gson.fromJson(response.getBody().asString(),ProductListingResultsModel.class);
             return productListingResultsModel;
         }
+
+        public ProductListingResultsModel getProductResultsAfterFilter(String searchTerm,String filterKey, String filterId,int k)
+        {
+            String createdFilter = null;
+            switch (filterKey.toLowerCase()) {
+                case "category":
+                    createdFilter = "filter_category[]";
+                    break;
+                case "price":
+                    createdFilter = "filter_price[]";
+                    break;
+                case "discount":
+                    createdFilter = "filter_discount[]";
+                    break;
+            }
+            response = shopUpPostMan.
+                    getCall(EndPoints.SEARCH_FOR_USER +
+                            "term=" + searchTerm +
+                            "&page=" + k + "&" + createdFilter + "=" + filterId);
+            ProductListingResultsModel productListingResultsModel =
+                    gson.fromJson(response.getBody().asString(), ProductListingResultsModel.class);
+            return  productListingResultsModel;
+        }
+
+    public Map<String,Object> getValidProductWithFilterOnly(String searchTerm,String filterKey, String filterId,int k)
+    {   Map<String, Object> productDetailsMap = new HashMap<>();
+        ProductListingResultsModel productListingResultsModel = getProductResultsAfterFilter(searchTerm,filterKey,filterId,k);
+
+        for (int i = 0; i < productListingResultsModel.getResults().size(); i++) {
+            List<ProductListingResultsModel.ResultsBean.SizesBean> productSizes =
+                    productListingResultsModel.getResults().get(i).getSizes();
+            for (int j = 0; j < productSizes.size(); j++) {
+                if (productSizes.get(j).getCount_on_hand() > 0 &&
+                        productSizes.get(j).getCorrected_count_on_hand() > 0) {
+                    // This is a product with valid
+                    productDetailsMap.put("ValidProductIndex", i);
+                    productDetailsMap.put("ValidProductDetails", productListingResultsModel.getResults().get(i));
+                    productDetailsMap.put("ValidSizeIndex", j);
+                    productDetailsMap.put("ProductCount",productListingResultsModel.getProducts_count());
+                    System.out.println(productListingResultsModel.getResults().get(i));
+                    return productDetailsMap;
+                }
+            }
+        }
+        return null;
+
+    }
 
 
 
