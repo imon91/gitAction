899687cd2b964d6704d4bb19b966a8a251b1612d
JagadeeshShopf <jagadeesh@shopf.co>
2020-07-25 19:13:42 +0530

    Domain Name Fix in webui AuthBaseClass, Implemented UnImplemented methods in ServiceRequestLayer of webui

--- webui/src/main/java/utils/ServiceRequestLayer.java
+++ webui/src/main/java/utils/ServiceRequestLayer.java
@@ -2,7 +2,7 @@ package utils;
 
 import coreUtils.CoreConstants;
 import services.commerceInterfaces.CommerceServices;
-import services.commerceMethods.GetCommerceApiResponse;
+import services.commerceMethods.*;
 
 public class ServiceRequestLayer implements CommerceServices {
 
@@ -16,6 +16,11 @@ public class ServiceRequestLayer implements CommerceServices {
     @Override
     public GetCommerceApiResponse getControlOverServices() {
         return new GetCommerceApiResponse(module);
+    }
+
+    @Override
+    public GetMyBagApiResponse getMyBagControl() {
+        return null;
     }
 
     @Override
@@ -36,6 +41,21 @@ public class ServiceRequestLayer implements CommerceServices {
     @Override
     public void getControlOverMyCartApi() {
 
+    }
+
+    @Override
+    public GetAuthenticationApiResponse getControlOverAuthentication() {
+        return null;
+    }
+
+    @Override
+    public GetSearchSuggestionsApiResponse getControlOverSearchSuggestionsApi() {
+        return null;
+    }
+
+    @Override
+    public GetPLPModuleApiResponse getControlOverPLPModuleApiResponse() {
+        return null;
     }
 
 }
