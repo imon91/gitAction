361ba2ed15ed50b52bfa6ca6cb1b0bad296454f8
JagadeeshShopf <jagadeesh@shopf.co>
2020-08-19 00:47:47 +0530

    added listeners for all modules

--- automation_report/src/main/java/PublishReport.java
+++ automation_report/src/main/java/PublishReport.java
@@ -1,41 +1,31 @@
-package generator;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Test;
+
 import java.io.File;
 import java.io.FileOutputStream;
 
 public class PublishReport {
 
-    @BeforeSuite
-    public void beforeSuite(){
-        try{
-            Runtime.getRuntime().exec("http-server -S -C localhost+3.pem -K localhost+3-key.pem");
-        }catch (Exception e){
-            System.out.println("Exception here" + e);
-        }
-        System.out.println("Called here");
+
+    public static void main(String[] args) throws Exception{
         WebDriverManager.chromedriver().setup();
-    }
-
-
-    @Test
-    public void publish() throws Exception{
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless");
+        //chromeOptions.addArguments("--headless");
         chromeOptions.setAcceptInsecureCerts(true);
         WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("https://127.0.0.1:8080/automation_report/src/main/java/index.html");
+        driver.get("https://automation-report.herokuapp.com/index.html");
+        for(int i=0;i<2;i++){
+            Thread.sleep(5000);
+            driver.navigate().refresh();
+        }
+        Thread.sleep(10000);
         System.out.println(driver.getCurrentUrl());
         String htmlData = driver.findElement(By.id("parent-tag")).getAttribute("outerHTML");
-        System.out.println(htmlData);
-        System.out.println("Total Tests Count : "+driver.findElement(By.id("tests-count")).getText());
+        //System.out.println(htmlData);
+        //System.out.println("Total Tests Count : "+driver.findElement(By.id("tests-count")).getText());
         driver.quit();
         File htmlTemplateFile = new File(System.getProperty("user.dir")+"/src/main/java/finalReports/daily_report.html");
         FileOutputStream fileOutputStream = new FileOutputStream(htmlTemplateFile);
@@ -44,9 +34,4 @@ public class PublishReport {
         //Runtime.getRuntime().exec("ps aux && pid=$(pgrep Python) && kill -9 $pid");
         //System.out.println("Done");
     }
-
-    @AfterSuite
-    public void killAllProcess() throws Exception{
-
-        }
 }
---
+++ mobilewebui/src/main/java/listeners/MobileWebUiTestNgListener.java
@@ -0,0 +1,110 @@
+package listeners;
+
+import coreUtils.BuildParameterKeys;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.json.simple.JSONObject;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.util.Date;
+
+import static io.restassured.RestAssured.given;
+
+public class MobileWebUiTestNgListener implements ITestListener {
+
+    private static class Keys{
+        public static final String KEY_DATE = "date";
+        public static final String KEY_DURATION = "duration";
+        public static final String KEY_TESTS = "total_tests";
+        public static final String KEY_PASS_COUNT = "pass_count";
+        public static final String KEY_FAIL_COUNT = "fail_count";
+        public static final String KEY_SKIP_COUNT = "skip_count";
+        public static final String KEY_PASS_PERCENTAGE = "pass_percentage";
+        public static final String KEY_FAIL_PERCENTAGE = "fail_percentage";
+        public static final String KEY_SKIP_PERCENTAGE = "skip_percentage";
+        public static final String KEY_FILTER_DATE = "filter_date";
+
+    }
+
+    public void onTestStart(ITestResult iTestResult) {
+
+    }
+
+    public void onTestSuccess(ITestResult iTestResult) {
+
+    }
+
+    public void onTestFailure(ITestResult iTestResult) {
+
+    }
+
+    public void onTestSkipped(ITestResult iTestResult) {
+
+    }
+
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+
+    }
+
+    public void onStart(ITestContext iTestContext) {
+
+    }
+
+    public void onFinish(ITestContext iTestContext) {
+        String trigger = System.getProperty(BuildParameterKeys.KEY_TRIGGER);
+        String app = null;
+       if(System.getProperty(BuildParameterKeys.KEY_APP).equalsIgnoreCase("Reseller")){
+            app = "reseller";
+       }else {
+           app = "mokam";
+       }
+
+        if (trigger != null) {
+            if (trigger.equalsIgnoreCase("jenkins")) {
+                JSONObject object = new JSONObject();
+                if (trigger.equalsIgnoreCase("jenkins")) {
+                    Date date = iTestContext.getStartDate();
+                    String duration = Long.toString(iTestContext.getEndDate().getTime()
+                            - iTestContext.getStartDate().getTime());
+                    double total_tests = iTestContext.getAllTestMethods().length;
+                    double pass_count = iTestContext.getPassedTests().size();
+                    double fail_count = iTestContext.getFailedTests().size();
+                    double skip_count = iTestContext.getSkippedTests().size();
+                    double pass_percentage = (pass_count / total_tests) * 100;
+                    double fail_percentage = (fail_count / total_tests) * 100;
+                    double skip_percentage = (skip_count / total_tests) * 100;
+                    String day = date.toString().split(" ")[2];
+                    String filter_date = date.getYear()+1900+"-"+"0"+(date.getMonth()+1)+"-"+day;
+                    System.out.println(filter_date);
+
+                    try {
+
+                        object.put(Keys.KEY_DATE, String.valueOf(date));
+                        object.put(Keys.KEY_DURATION, duration);
+                        object.put(Keys.KEY_TESTS, total_tests);
+                        object.put(Keys.KEY_PASS_COUNT, pass_count);
+                        object.put(Keys.KEY_FAIL_COUNT, fail_count);
+                        object.put(Keys.KEY_SKIP_COUNT, skip_count);
+                        object.put(Keys.KEY_PASS_PERCENTAGE, pass_percentage);
+                        object.put(Keys.KEY_FAIL_PERCENTAGE, fail_percentage);
+                        object.put(Keys.KEY_SKIP_PERCENTAGE, skip_percentage);
+                        object.put(Keys.KEY_FILTER_DATE,filter_date);
+
+                        System.out.println(object);
+
+                        RestAssured.baseURI = "http://localhost:3010/";
+                        Response response = given().header("Content-Type","application/json").
+                                body(object.toJSONString()).post(app+"/");
+                        response.then().log().all();
+
+                    } catch (Exception e) {
+                        System.out.println("Error in Adding Data : " + e);
+                    }
+                }
+            }
+
+        }
+    }
+}
