bdb72ff4fdf97f8247fd5e0a36fcf6ba9f6feb75
Nikhil Justus <nikhil.j@shopf.co>
2020-08-26 22:27:23 +0530

    VIDEO

--- core/src/main/java/videoRecorder/ScreenRecorder.java
+++ core/src/main/java/videoRecorder/ScreenRecorder.java
@@ -59,8 +59,8 @@ public class ScreenRecorder extends org.monte.screenrecorder.ScreenRecorder {
                 .getDefaultConfiguration();
 
         screenRecorder = new ScreenRecorder(gc, captureSize,
-                new Format(MediaTypeKey, MediaType.FILE, MimeTypeKey,MIME_MP4),
-                new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, ENCODING_QUICKTIME_JPEG,
+                new Format(MediaTypeKey, MediaType.FILE, MimeTypeKey, MIME_AVI),
+                new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
                         CompressorNameKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE, DepthKey, 24, FrameRateKey,
                         Rational.valueOf(15), QualityKey, 1.0f, KeyFrameIntervalKey, 15 * 60),
                 new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, "black", FrameRateKey, Rational.valueOf(30)),
