f682fc4b364672b7dd4608887aea902f32d3b4e3
nikhil-justus <65269635+nikhil-justus@users.noreply.github.com>
2020-09-17 15:41:46 +0530

    Update GetDriverFromCore.java

--- core/src/main/java/helper/GetDriverFromCore.java
+++ core/src/main/java/helper/GetDriverFromCore.java
@@ -145,6 +145,7 @@ public class GetDriverFromCore {
 
             ChromeOptions chromeOptions = new ChromeOptions();
             chromeOptions.setAcceptInsecureCerts(true);
+            chromeOptions.addArguments("--headless");
             chromeOptions.addArguments("--no-sandbox"); 
             chromeOptions.addArguments("--disable-dev-shm-usage");
             Map<String,Object> prefs = new HashMap<>();
