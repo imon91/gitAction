ade1de770fad543751d622a9dae4e75a5f7a02d1
JagadeeshShopf <jagadeesh@shopf.co>
2020-10-31 13:46:55 +0530

    Changed end-points

--- mobilewebui/src/main/java/utils/WAPConstants.java
+++ mobilewebui/src/main/java/utils/WAPConstants.java
@@ -3,32 +3,32 @@ package utils;
 public class WAPConstants {
 
 
-    public static final String AUTHENTICATION = "https://shopups2.xyz/authorize?ref=%2Fr";
-    public static final String MY_SHOP = "https://shopups2.xyz/wishlist";
-    public static final String MY_ORDERS = "https://shopups2.xyz/orders/myorders";
-    public static final String MY_INCOME ="https://shopups2.xyz/myaccount/income";
-    public static final String MY_COLLECTION_PAGE = "https://shopups2.xyz/users/122694";
+    public static final String AUTHENTICATION = "https://uatwap.shopups2.xyz/authorize?ref=%2Fr";
+    public static final String MY_SHOP = "https://uatwap.shopups2.xyz/wishlist";
+    public static final String MY_ORDERS = "https://uatwap.shopups2.xyz/orders/myorders";
+    public static final String MY_INCOME ="https://uatwap.shopups2.xyz/myaccount/income";
+    public static final String MY_COLLECTION_PAGE = "https://uatwap.shopups2.xyz/users/122694";
     public static final String MY_ORDER_TRACK = "/track";
-    public static final String MY_INCOME_LIST = "https://shopups2.xyz/myaccount/income/list";
-    public static final String MY_INCOME_LIST_DETAILS = "https://shopups2.xyz/myaccount/income/list/details";
-    public static final String MY_INCOME_TYPE_DELIVERED = "https://shopups2.xyz/myaccount/income/list?transaction_id=payment_pending&type=delivered";
-    public static final String MY_INCOME_TYPE_LOST = "https://shopups2.xyz/myaccount/income/list?transaction_id=payment_pending&type=lost";
-    public static final String MY_INCOME_TYPE_RTO = "https://shopups2.xyz/myaccount/income/list?transaction_id=payment_pending&type=rto";
-    public static final String MY_INCOME_TYPE_RETURNED = "https://shopups2.xyz/myaccount/income/list?transaction_id=payment_pending&type=returned";
-    public static final String MY_INCOME_TYPE_CANCELLED = "https://shopups2.xyz/myaccount/income/list?transaction_id=payment_pending&type=vendor_cancelled";
-    public static final String MY_INCOME_TYPE_BONUS = "https://shopups2.xyz/myaccount/income/list?transaction_id=payment_pending&type=bonus";
-    public static final String MY_INCOME_TYPE_ADJUSTMENTS = "https://shopups2.xyz/myaccount/income/list?transaction_id=payment_pending&type=adjustments";
-    public static final String MY_ACCOUNT = "https://shopups2.xyz/myaccount/personal_information";
-    public static final String ORDER_SUMMARY = "https://shopups2.xyz/orders/summary";
-    public static final String RESELLER_POLICY = "https://shopups2.xyz/policies";
-    public static final String PRIVACY_POLICY = "https://shopups2.xyz/privacy";
-    public static final String CONTACT_US = "https://shopups2.xyz/contactus";
-    public static final String SEARCH_FOR_TERM = "https://shopups2.xyz/search_terms/search_for_user?term=";
-    public static final String RECOMMENDATIONS = "https://shopups2.xyz/recommendations";
-    public static final String MY_BAG = "https://shopups2.xyz/checkout/bag";
-    public static final String CHECKOUT_ADDRESS = "https://shopups2.xyz/checkout/address";
-    public static final String CHECKOUT_PAYMENT = "https://shopups2.xyz/checkout/payment";
-    public static final String ORDER_SUCCESSFUL = "https://shopups2.xyz/checkout/thankyou";
+    public static final String MY_INCOME_LIST = "https://uatwap.shopups2.xyz/myaccount/income/list";
+    public static final String MY_INCOME_LIST_DETAILS = "https://uatwap.shopups2.xyz/myaccount/income/list/details";
+    public static final String MY_INCOME_TYPE_DELIVERED = "https://uatwap.shopups2.xyz/myaccount/income/list?transaction_id=payment_pending&type=delivered";
+    public static final String MY_INCOME_TYPE_LOST = "https://uatwap.shopups2.xyz/myaccount/income/list?transaction_id=payment_pending&type=lost";
+    public static final String MY_INCOME_TYPE_RTO = "https://uatwap.shopups2.xyz/myaccount/income/list?transaction_id=payment_pending&type=rto";
+    public static final String MY_INCOME_TYPE_RETURNED = "https://uatwap.shopups2.xyz/myaccount/income/list?transaction_id=payment_pending&type=returned";
+    public static final String MY_INCOME_TYPE_CANCELLED = "https://uatwap.shopups2.xyz/myaccount/income/list?transaction_id=payment_pending&type=vendor_cancelled";
+    public static final String MY_INCOME_TYPE_BONUS = "https://uatwap.shopups2.xyz/myaccount/income/list?transaction_id=payment_pending&type=bonus";
+    public static final String MY_INCOME_TYPE_ADJUSTMENTS = "https://uatwap.shopups2.xyz/myaccount/income/list?transaction_id=payment_pending&type=adjustments";
+    public static final String MY_ACCOUNT = "https://uatwap.shopups2.xyz/myaccount/personal_information";
+    public static final String ORDER_SUMMARY = "https://uatwap.shopups2.xyz/orders/summary";
+    public static final String RESELLER_POLICY = "https://uatwap.shopups2.xyz/policies";
+    public static final String PRIVACY_POLICY = "https://uatwap.shopups2.xyz/privacy";
+    public static final String CONTACT_US = "https://uatwap.shopups2.xyz/contactus";
+    public static final String SEARCH_FOR_TERM = "https://uatwap.shopups2.xyz/search_terms/search_for_user?term=";
+    public static final String RECOMMENDATIONS = "https://uatwap.shopups2.xyz/recommendations";
+    public static final String MY_BAG = "https://uatwap.shopups2.xyz/checkout/bag";
+    public static final String CHECKOUT_ADDRESS = "https://uatwap.shopups2.xyz/checkout/address";
+    public static final String CHECKOUT_PAYMENT = "https://uatwap.shopups2.xyz/checkout/payment";
+    public static final String ORDER_SUCCESSFUL = "https://uatwap.shopups2.xyz/checkout/thankyou";
 
 
 }
