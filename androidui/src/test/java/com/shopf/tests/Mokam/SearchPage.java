6a03fc4ec6ad4aadef7667a5f2017b7b0333e86c
sivaram kumar2908 <sivaram.a@shopf.co>
2020-12-06 11:21:24 +0530

    Search page creation

---
+++ androidui/src/test/java/com/shopf/tests/Mokam/SearchPage.java
@@ -0,0 +1,11 @@
+package com.shopf.tests.Mokam;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebElement;
+import utils.AndroidBaseClass;
+
+public class SearchPage extends AndroidBaseClass {
+
+    private AndroidDriver<WebElement> androidDriver;
+
+}
