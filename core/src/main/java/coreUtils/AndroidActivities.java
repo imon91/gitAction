41ce01ac9699cceb1515899698cc33170f74ed10
sivaram kumar2908 <sivaram.a@shopf.co>
2020-12-21 12:14:08 +0530

    Activity change

--- core/src/main/java/coreUtils/AndroidActivities.java
+++ core/src/main/java/coreUtils/AndroidActivities.java
@@ -4,7 +4,7 @@ public class AndroidActivities {
 
     public static class resellerActivities{
         public static final String homeActivity = ".HomeActivity";
-        public static final String searchActivity = ".search.NewSearchActivity";
+        public static final String searchActivity = "com.shopup.reseller.search.NewSearchActivity";
         public static final String PLPActivity = "com.shopup.feed.ui.FeedActivity";
         public static final String PDPActivity = "com.shopup.feed.ui.FeedActivity";
     }
