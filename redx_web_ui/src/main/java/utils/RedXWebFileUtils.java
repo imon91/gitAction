7c21a6d3ba798b9538519b188fc6cedcce662985
r3aditya <r3aditya@gmail.com>
2021-03-18 10:41:54 +0530

    Pagewise Sanity Fixes

--- redx_web_ui/src/main/java/utils/RedXWebFileUtils.java
+++ redx_web_ui/src/main/java/utils/RedXWebFileUtils.java
@@ -3,7 +3,7 @@ package utils;
 public class RedXWebFileUtils {
 
     public static final String createParcelDataCsvFilePath = RedXWebFileRouters.testDataPath + "createParcelData.csv";
-    public static final String createBulkParcelDataCsvFilePath = RedXWebFileRouters.testDataPath + "createBulkParcelData.csv";
+    public static final String createBulkParcelDataCsvFilePath = RedXWebFileRouters.testDataPath + "createBulkParcelData.xlsx";
     public static final String addNewShopDataCsvFilePath = RedXWebFileRouters.testDataPath + "addNewShopData.csv";
     public static final String editShopDataCsvFilePath = RedXWebFileRouters.testDataPath + "editShopData.csv";
     public static final String addNewLocationDataCsvFilePath = RedXWebFileRouters.testDataPath + "addNewLocationData.csv";
