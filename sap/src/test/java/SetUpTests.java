cd22e69886df405db0744debf3520575dec1f1b4
r3aditya <r3aditya@gmail.com>
2021-04-15 11:26:48 +0530

    Slack Bug Automation Updates
    -Can't find order in SAP download option

---
+++ sap/src/test/java/SetUpTests.java
@@ -0,0 +1,42 @@
+import coreUtils.CoreConstants;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
+import org.testng.annotations.*;
+import pageObjects.SAPPanelPageObject;
+import utils.SapBaseClass;
+
+public class SetUpTests extends SapBaseClass {
+
+    private WebDriver driver;
+    private SAPPanelPageObject sapPanelPageObject;
+
+    @BeforeSuite(alwaysRun = true)
+    public void sapBeforeSuite() throws Exception
+    {
+        System.out.println("Sap Before Suite");
+        driver = getBaseDriver();
+        setImplicitWait(10000);
+        driver.get(getSapBaseUrl());
+        setImplicitWait(10000);
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().accept();
+        new WebDriverWait(driver,10).until(ExpectedConditions.alertIsPresent());
+        driver.switchTo().alert().accept();
+        driver.manage().window().maximize();
+        sapPanelPageObject = new SAPPanelPageObject(driver);
+    }
+
+    @Test(groups = {CoreConstants.GROUP_SMOKE, CoreConstants.GROUP_SANITY},priority = 1)
+    public void setUpTest()
+    {
+        System.out.println("Setting Up Tests");
+        sapPanelPageObject.performAuthentication("01401122188","6666");
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void sapWebAfterSuite()
+    {
+        System.out.println("Sap Web After Suite");
+        quitBaseDriver();
+    }
+}
